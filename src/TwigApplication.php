d64713f8bd115cc1b0c1fac42578ca088126c999
Sorin <sorin.badea91@gmail.com>
2014-04-09 14:17:26 +0300

    PSR4 migration & some changeS

---
+++ src/TwigApplication.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @author    Sorin Badea <sorin.badea91@gmail.com>
+ * @license   MIT license (see the license file in the root directory)
+ */
+
+namespace ThinFrame\Twig;
+
+use PhpCollection\Map;
+use ThinFrame\Applications\AbstractApplication;
+use ThinFrame\Applications\DependencyInjection\ContainerConfigurator;
+
+
+/**
+ * Class TwigApplication
+ *
+ * @package ThinFrame\Twig
+ * @since   0.1
+ */
+class TwigApplication extends AbstractApplication
+{
+    /**
+     * Get application name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'TwigApplication';
+    }
+
+    /**
+     * Get application parents
+     *
+     * @return AbstractApplication[]
+     */
+    public function getParents()
+    {
+        return [];
+    }
+
+    /**
+     * Set different options for the container configurator
+     *
+     * @param ContainerConfigurator $configurator
+     */
+    protected function setConfiguration(ContainerConfigurator $configurator)
+    {
+        $configurator->addResources(
+            [
+                'Resources/config/services.yml'
+            ]
+        );
+    }
+
+    /**
+     * Set application metadata
+     *
+     * @param Map $metadata
+     *
+     */
+    protected function setMetadata(Map $metadata)
+    {
+
+    }
+}
